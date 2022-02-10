caf150c32a52c716f6e46cb515357edf3db81aec
brokecollegekidwithaclothingobsession <75198764+brokecollegekidwithaclothingobsession@users.noreply.github.com>
2022-02-10 15:33:54 -0800

    Initial tailwind and layout adjustments

--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -1,11 +1,16 @@
+import { Center, Button } from "@chakra-ui/react";
+
 export default function Index() {
   return (
-    <div className="max-w-md mx-auto bg-sonrSecondary rounded-xl shadow-md overflow-hidden md:max-w-7xl md:max-h-full">
+    <div className="max-w-md mx-auto bg-sonrBlue rounded-xl shadow-md overflow-hidden md:max-w-7xl md:max-h-full">
       <div className="md:flex flex-col">
         <div className="p-8">
-          <div className="uppercase tracking-wide text-3xl text-center text-lightGrey font-semibold">SONR Highway SDK</div>
-            <a href="#" className="block mt-1 text-lg text-center leading-tight font-medium text-snowWhite hover:underline">SONR Documentation</a>
+          <div className="uppercase tracking-wide text-3xl text-center text-snowWhite font-semibold">SONR Highway SDK</div>
+            <a href="#" className="block mt-1 text-lg text-center leading-tight font-medium text-snowWhite hover:text-sonrSecondary">SONR Documentation</a>
             <p className="mt-2 text-center text-snowWhite">We built the batteries attached Blockahin, Network, and Wallet.</p>
+            <Center>
+            <Button className="mt-4 text-center text-sonrSecondary" colorScheme='gray' size='md'> Get Started </Button>
+            </Center>
           </div>
       </div>
     </div>
