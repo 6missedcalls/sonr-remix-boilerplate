90a95ea33bcbaf2285fb9ee87a1930833f4448f8
brokecollegekidwithaclothingobsession <75198764+brokecollegekidwithaclothingobsession@users.noreply.github.com>
2022-02-10 23:41:50 -0800

    update of footer color

--- app/routes/Components/Footer/SocialMedia.tsx
+++ app/routes/Components/Footer/SocialMedia.tsx
@@ -2,7 +2,7 @@ import { ButtonGroup, ButtonGroupProps, IconButton } from '@chakra-ui/react'
 import { FaGithub, FaLinkedin, FaTwitter, FaDiscord, } from 'react-icons/fa'
 
 export const SocialMediaLinks = (props: ButtonGroupProps) => (
-  <ButtonGroup variant="ghost" color="gray.600" {...props}>
+  <ButtonGroup variant="ghost" color="#0061FF" {...props}>
     <IconButton as="a" href="#" aria-label="LinkedIn" icon={<FaLinkedin fontSize="20px" />} />
     <IconButton as="a" href="#" aria-label="GitHub" icon={<FaGithub fontSize="20px" />} />
     <IconButton as="a" href="#" aria-label="Twitter" icon={<FaTwitter fontSize="20px" />} />
