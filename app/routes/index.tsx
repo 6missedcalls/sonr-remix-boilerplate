57f25e7ef00e9d84de42f0917a916764a22198f2
Ian Judd <iankimjudd@gmail.com>
2022-02-14 12:09:06 -0500

    added postcss config and made changes to build script to import themes from tailwind config

--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -1,14 +1,14 @@
 export default function Index() {
   return (
-  <div className="mx-auto max-w-screen-xl px-4 py-8">
-    <div className="text-center">
-      <h1 className="text-3xl fsont-bold decoration-sonrBlue">
-        Home Page
-      </h1>
-      <p className="text-xl">
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit sed do eiusmod
-      </p>
+    <div className="mx-auto max-w-screen-xl px-4 py-8">
+      <div className="text-center">
+        <h1 className="text-3xl font-bold text-primary-sonrBlue decoration-sonrBlue">
+          Home Page
+        </h1>
+        <p className="text-xl">
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit sed do eiusmod
+        </p>
+      </div>
     </div>
-  </div>
   );
 }
